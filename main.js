8dd1fd9bf0bcf45be4dbad08a64b37ebdf445115
Dehyro <mendezdehyro@gmail.com>
2022-02-09 12:32:11 -0500

    Se agregó el listener del main en el main.js y se corrigió un error en el index.html

---
+++ main.js
@@ -0,0 +1,45 @@
+// Preguntas:
+/**
+ * 1. Por qué 
+ */
+
+(function(){
+	self.Board = function(width, height){
+		this.width = width;
+		this.height = height;
+		this.playing = false;
+		this.game_over = false;
+		this.bars = [];
+        this.ball = null;
+	}
+
+	self.Board.prototype = {
+		get elements(){
+			var elements = this.bars;
+			elements.push(this.ball);
+			return elements;
+
+		}
+	}
+})();
+
+(function(){
+	self.BoardView = function(canvas,board){
+		this.canvas = canvas;
+		this.canvas.width = board.width;
+		this.canvas.height = board.height;
+		this.board = board;
+		this.ctx = canvas.getContext("2d");
+	}
+})();
+
+self.addEventListener("load", main);
+
+// main():
+function main(){
+
+	var board = new Board(800,400);
+    var canvas = document.getElementById('canvas');
+    var board_view = new BoardView(canvas, board);
+
+}
