f7e96c94f896329f62e7d04521fac03c426ab5d7
Dehyro <mendezdehyro@gmail.com>
2022-02-10 14:29:01 -0500

    Corrección - finalmnente se agregó la última pregunta en el main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
  * 3. Por qué las instancias de los objetos se ponen fuera de la función main?
  * 4. Por qué es util la POO cuando se crean funciones con este método?
  * 5. Que tan útiles son los prototypes para los objetos que se crean?
- * 6. 
+ * 6. Cómo funcionan los getters y los setters?
  */
 
 (function(){
